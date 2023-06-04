8fc87d59113901cc771e9343c14f2193666c9f2e
Ishika Kashyap <ishikakashyap2412@gmail.com>
2023-06-04 21:50:27 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Contact from "./Contacts";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <Contact/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Contacts.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react"
+import contactsData from './contact.json'
+
+ const ContactApp = () => {
+    const [contacts, setContacts] = useState([])
+    const [search, setSearch] = useState('');
+    const [filteredContacts, setFilteredContacts] = useState([]);
+    const [selectedContact, setSelectedContact] = useState(null)
+
+    useEffect(() => {
+        setContacts(contactsData);
+      }, []);
+    
+      useEffect(() => {
+        const filtered = contacts.filter((contact) =>
+          contact.name.toLowerCase().includes(search.toLowerCase())
+        );
+        setFilteredContacts(filtered);
+      }, [search, contacts]);
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  }
+
+  const handleContactClick = (contact) => {
+    setSelectedContact(contact);
+  }
+
+  const handleClosePopup = () => {
+    setSelectedContact(null);
+  }
+
+
+    return (
+         <div>
+            <center>
+            <h1><b>CONTACT APP</b></h1>
+            </center>
+             <input
+                 type="text"
+                 placeholder="Search Contacts..."
+                 value={search}
+                 onChange={handleSearch}
+             />
+             <ul>    
+                {filteredContacts.map(contact=>(
+                    <li key={contact.id} onClick={() => handleContactClick(contact)}> 
+                        <p>Name: {contact.name}</p>
+                        <p>Phone: {contact.phone}</p>
+                    </li>
+                ))}
+            </ul>   
+            {selectedContact && (
+        <div className="popup">
+          <div className="popup-content">
+            <h2>Contact Details</h2>
+            <p>Name: {selectedContact.name}</p>
+            <p>Number: {selectedContact.number}</p>
+            <button onClick={handleClosePopup}>Close</button>
+          </div>
+        </div>
+      )}
+        </div>
+    )
+}
+
+ export default ContactApp;
